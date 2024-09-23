e639fcffb7b64f01683fb7acbff53499f3ad121f
hadxr <hadikhan05@hotmail.co.uk>
2024-09-23 03:04:36 +0100

    Added Main.py, textnode.py. Added Testing, gitignore, test/run main shell. Added baseplate HTML & CSS

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    testNode = TextNode('hello','md','https://hello')
+    print(testNode)
+
+main()
+
---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_property(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+        self.assertNotEqual(node,node2)
+
+    def test_Instance(self):
+        node = TextNode("","","")
+        node2 = TextNode("","","")
+        self.assertEqual(node,node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
